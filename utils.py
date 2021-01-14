78845d6f9f53905fbd79b7bdee09cd18378ae4ad
Anastasiia <45288351+ansafo@users.noreply.github.com>
2021-01-14 23:37:27 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,6 @@
 Mask R-CNN
 Common utility functions and classes.
 
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
 """
 
 import sys
